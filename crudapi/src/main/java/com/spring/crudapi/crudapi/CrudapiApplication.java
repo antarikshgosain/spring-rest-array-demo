24e4963c32ed84341fb3d17aa01fcd91c8240625
antariksh39 <gosain.antariksh@gmail.com>
2023-08-23 21:08:58 +0530

    swagger ui added

--- crudapi/src/main/java/com/spring/crudapi/crudapi/CrudapiApplication.java
+++ crudapi/src/main/java/com/spring/crudapi/crudapi/CrudapiApplication.java
@@ -2,7 +2,14 @@ package com.spring.crudapi.crudapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class CrudapiApplication {
 
--- crudapi/src/main/java/com/spring/crudapi/crudapi/util/FruitUtil.java
+++ crudapi/src/main/java/com/spring/crudapi/crudapi/util/FruitUtil.java
@@ -2,13 +2,6 @@ package com.spring.crudapi.crudapi.util;
 
 public class FruitUtil {
 
-    public static boolean isPositive (int num) {
-
-        if( num>0 ){
-
-        }
-
-        return false;
-    }
+    /* Utility methods go here */
 
 }
